fa3d44feb7a17af65d9ac1f1bb43993ae1ecbc9a
totoogg <goncharov.vova2014@gamil.com>
2024-08-23 21:11:38 +0300

    feat: add google maps and form create station

--- src/app/features/admin/admin.module.ts
+++ src/app/features/admin/admin.module.ts
@@ -1,5 +1,8 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { GoogleMapsModule } from '@angular/google-maps';
 import { NavigationComponent } from '@features/admin/components';
 import {
   AdminComponent,
@@ -18,7 +21,13 @@ import { AdminRoutingModule } from './admin-routing.module';
     RoutesComponent,
     StationsComponent,
   ],
-  imports: [CommonModule, AdminRoutingModule],
+  imports: [
+    CommonModule,
+    AdminRoutingModule,
+    GoogleMapsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
   exports: [],
   providers: [],
 })
