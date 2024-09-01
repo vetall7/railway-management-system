3d12a367eed49b7aa49fd19bd8903fdd5ba5d635
Andrey Loginov <andrey12q112@gmail.com>
2024-09-01 05:49:14 +0300

    feat: add guest-only guard

--- src/app/features/auth/auth-routing.module.ts
+++ src/app/features/auth/auth-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { guestGuard } from '@shared/guards/guest.guard';
 
 import { SigninComponent } from './pages/signin/signin.component';
 import { SignupComponent } from './pages/signup/signup.component';
@@ -7,10 +8,12 @@ import { SignupComponent } from './pages/signup/signup.component';
 const routes: Routes = [
   {
     path: 'signup',
+    canMatch: [guestGuard],
     component: SignupComponent,
   },
   {
     path: 'signin',
+    canMatch: [guestGuard],
     component: SigninComponent,
   },
 ];
