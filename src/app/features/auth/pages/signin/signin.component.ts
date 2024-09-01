3bf4a3cb1a03259d2d56728c9ed3cd4022ff450e
Andrey Loginov <andrey12q112@gmail.com>
2024-09-01 05:49:59 +0300

    feat: better login handling, redirects etc

--- src/app/features/auth/pages/signin/signin.component.ts
+++ src/app/features/auth/pages/signin/signin.component.ts
@@ -1,8 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthError, AuthRes } from '@features/auth/models/auth.model';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { MessageService } from 'primeng/api';
+import { combineLatest, Observable } from 'rxjs';
 
 import * as AuthActions from '../../store/auth.action';
 import * as AuthSelectors from '../../store/auth.selector';
@@ -22,7 +24,11 @@ export class SigninComponent {
 
   authLoading$: Observable<boolean>;
 
-  constructor(private store: Store) {
+  constructor(
+    private store: Store,
+    private messageService: MessageService,
+    private router: Router,
+  ) {
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required]),
@@ -36,8 +42,26 @@ export class SigninComponent {
   onLogin() {
     if (this.loginForm.valid) {
       const { email, password } = this.loginForm.value;
-      this.store.dispatch(
-        AuthActions.signIn({ playload: { email, password } }),
+
+      this.store.dispatch(AuthActions.signIn({ payload: { email, password } }));
+
+      combineLatest([this.authError$, this.authResponse$]).subscribe(
+        ([error, response]) => {
+          if (error?.error.message) {
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Error',
+              detail: error.error.message,
+            });
+          } else if (response) {
+            this.router.navigate(['/']);
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Success',
+              detail: 'Login successful. Welcome!',
+            });
+          }
+        },
       );
     }
   }
--- src/app/features/auth/pages/signup/signup.component.ts
+++ src/app/features/auth/pages/signup/signup.component.ts
@@ -7,8 +7,11 @@ import {
   ValidatorFn,
   Validators,
 } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthError } from '@features/auth/models/auth.model';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { MessageService } from 'primeng/api';
+import { combineLatest, Observable } from 'rxjs';
 
 import * as AuthActions from '../../store/auth.action';
 import * as AuthSelectors from '../../store/auth.selector';
@@ -22,11 +25,17 @@ import * as AuthSelectors from '../../store/auth.selector';
 export class SignupComponent implements OnInit {
   registerForm: FormGroup;
 
-  authError$: Observable<unknown>;
+  authError$: Observable<AuthError | null>;
 
   authLoading$: Observable<boolean>;
 
-  constructor(private store: Store) {
+  authResponse$: Observable<unknown>;
+
+  constructor(
+    private store: Store,
+    private router: Router,
+    private messageService: MessageService,
+  ) {
     this.registerForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required]),
@@ -35,6 +44,7 @@ export class SignupComponent implements OnInit {
 
     this.authError$ = this.store.select(AuthSelectors.selectAuthError);
     this.authLoading$ = this.store.select(AuthSelectors.selectAuthLoading);
+    this.authResponse$ = this.store.select(AuthSelectors.selectAuthResponse);
   }
 
   ngOnInit(): void {
@@ -59,8 +69,26 @@ export class SignupComponent implements OnInit {
   onRegister() {
     if (this.registerForm.valid) {
       const { email, password } = this.registerForm.value;
-      this.store.dispatch(
-        AuthActions.signUp({ playload: { email, password } }),
+      this.store.dispatch(AuthActions.signUp({ payload: { email, password } }));
+
+      combineLatest([this.authError$, this.authResponse$]).subscribe(
+        ([error, response]) => {
+          if (error?.error.message) {
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Error',
+              detail: error.error.message,
+            });
+          } else if (response !== null) {
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Success',
+              detail: 'You can now sign in',
+            });
+
+            this.router.navigate(['/auth/signin']);
+          }
+        },
       );
     }
   }
