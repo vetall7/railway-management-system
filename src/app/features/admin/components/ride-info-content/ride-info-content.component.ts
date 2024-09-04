2a773b5cc6288224066cc7f07d6b4ab2c2fb9eb9
totoogg <goncharov.vova2014@gamil.com>
2024-09-04 17:37:24 +0300

    fix: method update ride time and price

--- src/app/features/admin/components/ride-info-content/ride-info-content.component.ts
+++ src/app/features/admin/components/ride-info-content/ride-info-content.component.ts
@@ -1,11 +1,15 @@
+/* eslint-disable operator-linebreak */
 import {
   ChangeDetectionStrategy,
   Component,
   DoCheck,
   EventEmitter,
+  inject,
   Input,
+  OnDestroy,
   OnInit,
   Output,
+  Renderer2,
   signal,
 } from '@angular/core';
 import { IDataRideChange, ISegmentsRide } from '@features/admin/models';
@@ -16,7 +20,7 @@ import { IDataRideChange, ISegmentsRide } from '@features/admin/models';
   styleUrl: './ride-info-content.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RideInfoContentComponent implements OnInit, DoCheck {
+export class RideInfoContentComponent implements OnInit, DoCheck, OnDestroy {
   @Input() index: number | null = -1;
 
   @Input() name: string | undefined = '';
@@ -25,13 +29,29 @@ export class RideInfoContentComponent implements OnInit, DoCheck {
 
   @Input() segment: ISegmentsRide | undefined = { time: [], price: {} };
 
+  @Input() segmentPrev: ISegmentsRide | undefined = { time: [], price: {} };
+
+  @Input() segmentNext: ISegmentsRide | undefined = { time: [], price: {} };
+
   @Input() create: boolean | undefined = false;
 
   @Input() carriagesName: (string | undefined)[] = [];
 
+  @Input() openEditTimeForm = false;
+
+  @Input() openEditPriceForm = false;
+
   @Output() changed = new EventEmitter<IDataRideChange>();
 
   @Output() changedCreate = new EventEmitter<IDataRideChange>();
+
+  @Output() openEditTime = new EventEmitter<boolean>();
+
+  @Output() openEditPrice = new EventEmitter<boolean>();
+
+  private renderer = inject(Renderer2);
+
+  private bodyClickListener?: () => void;
 
   price = signal<[string, number][]>([]);
 
@@ -44,6 +64,18 @@ export class RideInfoContentComponent implements OnInit, DoCheck {
   priceSend = signal<[string, number][]>([]);
 
   check = signal<boolean>(false);
+
+  alert = signal<boolean>(false);
+
+  alertPrice = signal<boolean>(false);
+
+  alertText = signal<string>('');
+
+  alertTextPrice = signal<string>('');
+
+  checkPrevTime = signal<boolean>(true);
+
+  checkNextTime = signal<boolean>(true);
 
   ngOnInit(): void {
     if (this.create) {
@@ -59,43 +91,118 @@ export class RideInfoContentComponent implements OnInit, DoCheck {
       this.priceSend.set(Object.entries(this.segment?.price || {}));
       this.timeSend.set(this.segment?.time || []);
     }
+
+    this.bodyClickListener = this.renderer.listen(
+      // eslint-disable-next-line no-undef
+      document.body,
+      'click',
+      (event) => {
+        if (
+          !event.target.closest('.edit_time') &&
+          !event.target.closest('.save_time')
+        ) {
+          this.alert.set(false);
+        }
+        if (
+          !event.target.closest('.edit_price') &&
+          !event.target.closest('.save_price')
+        ) {
+          this.alertPrice.set(false);
+        }
+      },
+    );
   }
 
   handleClickEditTime() {
-    this.editTime.set(true);
+    if (!this.openEditTimeForm) {
+      this.editTime.set(true);
+      this.openEditTime.emit(true);
+    } else {
+      this.alertText.set('Please close time editing mode.');
+      this.alert.set(true);
+    }
   }
 
   handleClickEditPrice() {
-    this.editPrice.set(true);
+    if (!this.openEditPriceForm) {
+      this.editPrice.set(true);
+      this.openEditPrice.emit(true);
+    } else {
+      this.alertTextPrice.set('Please close price editing mode.');
+      this.alertPrice.set(true);
+    }
   }
 
   handleClickSaveTime() {
-    this.editTime.set(false);
-    const data = {
-      index: this.index!,
-      time: this.timeSend(),
-      price: Object.fromEntries(this.price()),
-    };
-    this.changed.emit(data);
+    if (this.segmentPrev) {
+      const a = [...this.segmentPrev.time, ...this.timeSend()].map((el) =>
+        new Date(el).getTime(),
+      );
+      const b = a
+        .slice(1)
+        .map((el, i) => el - a[i])
+        .every((el) => el > 0);
+      this.checkPrevTime.set(b);
+    }
+    if (this.segmentNext) {
+      const a = [...this.timeSend(), ...this.segmentNext.time].map((el) =>
+        new Date(el).getTime(),
+      );
+      const b = a
+        .slice(1)
+        .map((el, i) => el - a[i])
+        .every((el) => el > 0);
+      this.checkNextTime.set(b);
+    }
+    if (this.checkNextTime() && this.checkPrevTime()) {
+      this.editTime.set(false);
+      const data = {
+        index: this.index!,
+        time: this.timeSend(),
+        price: Object.fromEntries(this.price()),
+      };
+      this.changed.emit(data);
+      this.openEditTime.emit(false);
+    } else {
+      this.alertText.set(
+        'At each station, the departure time must be greater than the arrival time, and the arrival time at each station must be greater than the departure time from the previous station.',
+      );
+      this.alert.set(true);
+    }
   }
 
   handleClickSavePrice() {
-    this.editPrice.set(false);
-    const data = {
-      index: this.index!,
-      time: this.timeSend(),
-      price: Object.fromEntries(this.priceSend()),
-    };
-    this.changed.emit(data);
+    const a = this.priceSend()
+      .flat()
+      .filter((el) => typeof el === 'number')
+      .every((el) => el > 0);
+    if (a) {
+      this.editPrice.set(false);
+      const data = {
+        index: this.index!,
+        time: this.timeSend(),
+        price: Object.fromEntries(this.priceSend()),
+      };
+      this.changed.emit(data);
+    } else {
+      this.alertTextPrice.set('The price must be greater than 0.');
+      this.alertPrice.set(true);
+    }
   }
 
   handleChangeTime(event: Event) {
     const target = event.target as HTMLInputElement;
     if (target.name === 'arrival') {
-      this.timeSend.update((el) => [`${target.value}:29.906Z`, el[1]]);
+      this.timeSend.update(() => [
+        `${target.value}:29.906Z`,
+        `${target.value}:30.906Z`,
+      ]);
     }
     if (target.name === 'departure') {
-      this.timeSend.update((el) => [el[0], `${target.value}:29.906Z`]);
+      this.timeSend.update(() => [
+        `${target.value}:28.906Z`,
+        `${target.value}:29.906Z`,
+      ]);
     }
     if (this.create) {
       if (this.pos === 'start') {
@@ -151,6 +258,12 @@ export class RideInfoContentComponent implements OnInit, DoCheck {
       !this.create
     ) {
       this.price.set(Object.entries(this.segment?.price || {}));
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.bodyClickListener) {
+      this.bodyClickListener();
     }
   }
 }
